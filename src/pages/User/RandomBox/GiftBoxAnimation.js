96e806a9b6d0a7f4dc9bf2ff3446c998ca3e15b7
rlawndks4 <rlawndks0423@naver.com>
2022-11-24 00:54:01 +0900

    asdf

--- src/pages/User/RandomBox/GiftBoxAnimation.js
+++ src/pages/User/RandomBox/GiftBoxAnimation.js
@@ -5,7 +5,9 @@ import kuku from "../../../assets/images/randombox/jump-character.png";
 // import ConfettiGenerator from "./CanvasConfetti";
 import Confetti from "./confetti/Confetti.js";
 import { useNavigate } from "react-router-dom";
-
+import pointImg from '../../../assets/images/icon/home/point.svg';
+import { useState } from "react";
+import { logoSrc } from "../../../data/ContentData";
 const init_state = {
     move: "move",
     jump: "",
@@ -14,6 +16,7 @@ const init_state = {
 };
 const GiftBoxAnimation = () => {
     const navigate = useNavigate();
+    const [clickDouble, setClickDouble] = useState(false)
     const [state, setState] = useReducer(
         (state, new_state) => ({
             ...state,
@@ -24,9 +27,9 @@ const GiftBoxAnimation = () => {
 
     const { move, rotating, rotated, jump } = state;
 
-    async function animate() {
+    const animate = async () =>{
         let isDone = rotated === "rotated" ? true : false;
-
+        let click = false;
         if (!isDone) {
             setState({ rotating: "rotating" });
             setTimeout(() => {
@@ -35,36 +38,40 @@ const GiftBoxAnimation = () => {
             setTimeout(() => {
                 setState({ rotated: "rotated" });
             }, 1000);
+            await new Promise((r) => setTimeout(r, 3000));
+            if(!clickDouble){
+                if (window.confirm('당첨되었습니다. 홈으로 이동하시겠습니까?2')) {
+                    navigate('/home');
+                    return;
+                }
+            }
         } else {
-            if (window.confirm('당첨되었습니다. 홈으로 이동하시겠습니까?')) {
+            if (window.confirm('당첨되었습니다. 홈으로 이동하시겠습니까?1')) {
+                setClickDouble(true);
                 navigate('/home');
+                return;
             }
         }
         let moving = move === "move" ? "" : "move";
         setState({ move: moving });
-        if (!isDone) {
-            await new Promise((r) => setTimeout(r, 3000));
-            if (window.confirm('당첨되었습니다. 홈으로 이동하시겠습니까?')) {
-                navigate('/home');
-            }
-        }
-
     }
 
     return (
         <div className="App">
             <Confetti open={jump === "jump"} />
             <div className="img-container">
-                <img className={`kuku ${jump}`} src={kuku} alt="kuku" />
-                <button className="box" onClick={() => animate()}>
-                    <img src={box} alt="box" />
+                <img className={`kuku ${jump}`} src={pointImg} alt="kuku" style={{width:'72px'}} />
+                <button className="box" onClick={animate}>
+                <img src={box} alt="box" />
                 </button>
                 <img
                     className={`lid ${move} ${rotating} ${rotated}`}
                     src={boxLid}
                     alt="box-lid"
                 />
+                <img src={logoSrc} style={{width:'84px'}} onClick={()=>navigate('/home')} />
             </div>
+            
         </div>
     );
 }
